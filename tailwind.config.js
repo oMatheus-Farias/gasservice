0a09401129cb30a55c9294f768edc546090e5951
unknown <matheusfariasdasneves@gmail.com>
2023-12-24 17:44:38 -0300

    feat: adiciona variaveis de cores no tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#A30F13',
+        redColor: '#ED3237',
+        offWhite: '#E8E8E8',
+        whiteColor: '#FFF',
+      },
+    },
   },
   plugins: [],
 }
