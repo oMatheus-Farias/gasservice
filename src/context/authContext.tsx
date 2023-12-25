37fcabd3911877046cf04d5e92d19b5071d636be
unknown <matheusfariasdasneves@gmail.com>
2023-12-24 22:39:45 -0300

    docs: adiciona firebase

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,14 +1,33 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+
+interface UserProps{
+  uid: string | null,
+  name: string | null,
+  email: string | null,
+  password: string | null,
+};
 
 interface AuthContextData{
+  signed: boolean,
+  user: UserProps | null,
+};
 
+interface SignInUserProps{
+  email: string,
+  password: string,
 };
 
 export const AuthContext = createContext({} as AuthContextData);
 
 export default function AuthProvider({ children }: { children: ReactNode }){
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  // function SignIn({ email, password }: SignInUserProps){
+
+  // };
+
   return(
-    <AuthContext.Provider value={{  }} >
+    <AuthContext.Provider value={{ signed: !!user, user }} >
       { children }
     </AuthContext.Provider>
   );
