7073e9933200a06445dcd4d8dfd4c96a8685b1d4
unknown <matheusfariasdasneves@gmail.com>
2023-12-26 18:48:30 -0300

    feat: adiciona componente CardProps

---
+++ src/components/cardProduct/index.tsx
@@ -0,0 +1,22 @@
+interface CardProps{
+  image: string,
+  alternative: string,
+  title: string,
+  description: string,
+};
+
+export default function CardProduct({ image, alternative, title, description }: CardProps){
+  return(
+    <section className='mx-auto' >
+      <img
+        src={ image }
+        alt={ alternative }
+      />
+
+      <div className='max-w-[25em]' >
+        <h2 className='mt-3 text-redColor' >{ `${title}:` }</h2>
+        <p className='text-whiteColor text-justify' >{ description }</p>
+      </div>
+    </section>
+  );
+};
