45392929ee6fb8d4a6bc57a5d52255284348b836
unknown <matheusfariasdasneves@gmail.com>
2023-12-25 21:37:41 -0300

    feat: adiciona componente Header mobile e dasktop em todas as demais paginas do projeto

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,29 @@
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../context/authContext';
+
+import Background from '../../components/background';
+import HeaderMobile from '../../components/headerMobile';
+import HeaderDasktop from '../../components/headerDasktop';
+import Container from '../../components/container';
+
 export default function Products(){
+  const { screenSize, setActivePageIndicator } = useContext(AuthContext);
+
+  useEffect(() => {
+    setActivePageIndicator('/products');
+
+    return () => {
+      setActivePageIndicator('');
+    };
+  }, []);
+
   return(
-    <div>
-      <h1>Page Products</h1>
-    </div>
+    <Background>
+      { screenSize !== null ? screenSize ? <HeaderDasktop/> : <HeaderMobile/> : ''}
+
+      <Container>
+        <h1>Page Products</h1>
+      </Container>
+    </Background>
   );
 };
--- src/pages/whoWeAre/index.tsx
+++ src/pages/whoWeAre/index.tsx
@@ -1,7 +1,29 @@
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../context/authContext';
+
+import Background from '../../components/background';
+import HeaderMobile from '../../components/headerMobile';
+import HeaderDasktop from '../../components/headerDasktop';
+import Container from '../../components/container';
+
 export default function WhoWeAre(){
+  const { screenSize, setActivePageIndicator } = useContext(AuthContext);
+
+  useEffect(() => {
+    setActivePageIndicator('/whoweare');
+
+    return () => {
+      setActivePageIndicator('');
+    };
+  }, []);
+
   return(
-    <div>
-      <h1>Page WhoWeAre</h1>
-    </div>
+    <Background>
+      { screenSize !== null ? screenSize ? <HeaderDasktop/> : <HeaderMobile/> : ''}
+
+      <Container>
+        <h1>Page WhoWeAre</h1>
+      </Container>
+    </Background>
   );
 };
