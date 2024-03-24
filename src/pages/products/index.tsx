8fdb7abc91b9037d26f2348bb4513e085f2b5975
Matheus <matheusfariasdasneves@gmail.com>
2024-03-24 19:48:08 -0300

    refactor: ensure that the background color of products is not white and limit their maximum width

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,51 +1,65 @@
-import { useContext, useEffect } from 'react';
-import { AuthContext } from '../../context/authContext';
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/authContext";
 
-import Background from '../../components/background';
-import HeaderMobile from '../../components/headerMobile';
-import HeaderDasktop from '../../components/headerDasktop';
-import Container from '../../components/container';
-import NavMobile from '../../components/navMobile';
-import CardProduct from '../../components/cardProduct';
+import Background from "../../components/background";
+import HeaderMobile from "../../components/headerMobile";
+import HeaderDasktop from "../../components/headerDasktop";
+import Container from "../../components/container";
+import NavMobile from "../../components/navMobile";
+import CardProduct from "../../components/cardProduct";
 
-export default function Products(){
-  const { screenSize, setActivePageIndicator, openCloseNav, setOpenCloseNav, products } = useContext(AuthContext);
+export default function Products() {
+  const {
+    screenSize,
+    setActivePageIndicator,
+    openCloseNav,
+    setOpenCloseNav,
+    products,
+  } = useContext(AuthContext);
 
   useEffect(() => {
-    setActivePageIndicator('/products');
+    setActivePageIndicator("/products");
     setOpenCloseNav(false);
 
     return () => {
-      setActivePageIndicator('');
+      setActivePageIndicator("");
     };
   }, []);
 
-  return(
+  return (
     <Background>
-      { screenSize !== null ? screenSize ? <HeaderDasktop/> : <HeaderMobile/> : ''}
+      {screenSize !== null ? (
+        screenSize ? (
+          <HeaderDasktop />
+        ) : (
+          <HeaderMobile />
+        )
+      ) : (
+        ""
+      )}
 
       <Container>
-        <h1 className='mt-8 text-whiteColor text-2xl text-center font-semibold' >
+        <h1 className="mt-8 text-whiteColor text-2xl text-center font-semibold">
           Produtos
-          <span className='text-redColor' >:</span>
+          <span className="text-redColor">:</span>
         </h1>
 
-        <div className='mt-8 w-full grid grid-cols-1 gap-7 lg:grid-cols-2 xl:grid-cols-3' >
-          { products.map((item, index) => {
-            return(
-              <div className='mx-auto' key={ index } >
+        <div className="mt-8 w-full grid grid-cols-1 gap-7 lg:grid-cols-2 xl:grid-cols-3 max-w-[1300px] mx-auto">
+          {products.map((item, index) => {
+            return (
+              <div className="mx-auto" key={index}>
                 <CardProduct
-                  image={ item.image }
-                  alternative={ item.alternative }
-                  title={ item.title }
-                  description={ item.description }
+                  image={item.image}
+                  alternative={item.alternative}
+                  title={item.title}
+                  description={item.description}
                 />
               </div>
-            )
-          }) }
+            );
+          })}
         </div>
       </Container>
-      { !screenSize && openCloseNav && <NavMobile/> }
+      {!screenSize && openCloseNav && <NavMobile />}
     </Background>
   );
-};
+}
